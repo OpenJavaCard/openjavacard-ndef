e2e1b4588b05d5a5a306743ed6d4a07a8648f9b4
Ingo Albrecht <prom@berlin.ccc.de>
2018-02-27 10:36:54 +0100

    Moved full version to its own package

--- src/full/org/aispring/javacard/ndef/full/NdefApplet.java
+++ src/full/org/aispring/javacard/ndef/full/NdefApplet.java
@@ -17,7 +17,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
  */
 
-package org.aispring.javacard.ndef;
+package org.aispring.javacard.ndef.full;
 
 import javacard.framework.*;
 
